5dc46b586d66bc5b66f08091292cead3dab35066
Teemu Mähönen <teemu.mahonen@geniem.com>
2018-03-13 13:49:52 +0200

    Refactored Button component and added examples.

--- Components/Button/Styles.js
+++ Components/Button/Styles.js
@@ -1,15 +1,50 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
 import { Colors } from '../../Themes'
 
 export default StyleSheet.create({
-  button: {
+  primaryButton: {
+    backgroundColor: Colors.violet,
+    height: 50,
+    width: '66%',
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
     alignSelf: 'center'
   },
-  buttonText: {
+  secondaryButton: {
+    backgroundColor: Colors.white,
+    height: 50,
+    width: '66%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderWidth: 2,
+    borderColor: Colors.violet
+  },
+  primaryText: {
     color: Colors.white,
     backgroundColor: Colors.transparent,
     textAlign: 'center',
     fontWeight: 'bold'
+  },
+  secondaryText: {
+    color: Colors.violet,
+    backgroundColor: Colors.transparent,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+  disabled: {
+    position: 'absolute',
+    top: Platform.OS === 'ios' ? -15 : 0,
+    backgroundColor: Colors.disabled,
+    width: '100%',
+    height: '100%'
+  },
+  flex: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
--- Themes/Colors.js
+++ Themes/Colors.js
@@ -2,7 +2,11 @@ const colors = {
   white: '#ffffff',
   black: '#000000',
   transparent: 'rgba(0,0,0,0)',
-  grey: '#aaa'
+  grey: '#aaa',
+  violet: '#780076',
+  primaryUnderlay: '#540052',
+  secondaryUnderlay: '#b2b2b2',
+  disabled: 'rgba(0,0,0,.2)'
 }
 
 export default colors
