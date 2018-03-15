3c7229d7a07581b0db3180eceda37baab9d06219
Matias Mäkitalo <matias.makitalo@geniem.com>
2018-03-15 11:00:42 +0200

    Add animation to dropdown picker.

--- Components/DropdownPicker/DropdownPicker.js
+++ Components/DropdownPicker/DropdownPicker.js
@@ -1,13 +1,36 @@
 import React, { PureComponent } from 'react'
-import { FlatList, View, Text, TouchableOpacity } from 'react-native'
+import { FlatList, View, Text, TouchableOpacity, Animated, Easing } from 'react-native'
 import { Colors } from '../../Themes'
 import styles from './Styles'
 
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
+
 export default class DropdownPicker extends PureComponent {
+  constructor (props) {
+    super(props)
+    const animatedValueInit = props.visible ? 1 : 0
+    this.animatedValue = new Animated.Value(animatedValueInit)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    /* Start animation when component receives next props. */
+    const nextVal = nextProps.visible ? 1 : 0
+    const duration = nextProps.animationDuration != null ? nextProps.animationDuration : 150
+
+    Animated.timing(
+      this.animatedValue,
+      {
+        toValue: nextVal,
+        duration,
+        easing: Easing.inOut(Easing.quad)
+      }
+    ).start()
+  }
+
   keyExtractor = (item, index) => index.toString()
 
   renderItem = ({item, index}) => {
-    const style = { alignSelf: 'center', justifyContent: 'center', alignItems: 'center', height: 45, width: '80%' }
+    const style = { alignSelf: 'center', justifyContent: 'center', alignItems: 'center', height: 45, width: '80%', backgroundColor: Colors.transparent }
     const textStyle = { fontSize: 18, color: Colors.violet }
     return (
       <TouchableOpacity style={style} onPress={() => this.props.onItemPress(item)}>
@@ -19,27 +42,36 @@ export default class DropdownPicker extends PureComponent {
   renderSeparator = () => this.props.separatorComponent || null
 
   renderItems = () => {
-    if (this.props.visible) {
-      const style = {
-        alignSelf: 'center',
-        width: '80%',
-        borderWidth: 2,
-        borderTopWidth: 0,
-        borderColor: Colors.violet,
-        height: 230
-      }
-      return (
-        <FlatList
-          style={style}
-          renderItem={this.renderItem}
-          keyExtractor={this.keyExtractor}
-          ItemSeparatorComponent={this.renderSeparator}
-          showsVerticalScrollIndicator={false}
-          {...this.props}
-        />
-      )
+    const dropdownHeight = this.props.dropdownHeight || 208
+
+    const style = {
+      alignSelf: 'center',
+      width: '80%',
+      borderWidth: 2,
+      borderTopWidth: 0,
+      borderColor: Colors.violet
     }
-    return null
+
+    /* Use animated height and opacity changes */
+    const height = this.animatedValue.interpolate({
+      inputRange: [0, 0.75],
+      outputRange: [0, dropdownHeight] // Use 'auto' as final value to enable container expanding
+    })
+    const opacity = this.animatedValue.interpolate({
+      inputRange: [0, 0.000001],
+      outputRange: [0, 1]
+    })
+
+    return (
+      <AnimatedFlatList
+        style={[style, {height, opacity}]}
+        renderItem={this.renderItem}
+        keyExtractor={this.keyExtractor}
+        ItemSeparatorComponent={this.renderSeparator}
+        showsVerticalScrollIndicator={false}
+        {...this.props}
+      />
+    )
   }
 
   render () {
